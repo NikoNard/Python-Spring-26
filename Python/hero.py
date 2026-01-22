55f3d0950e8a8cfdada4c542f50622327860d08f
NikoNard <nikonardulli@gmail.com>
2026-01-22 15:56:47 -0600

    Add files via upload

---
+++ Python/hero.py
@@ -0,0 +1,32 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+Please ensure your code meets the following before submitting:
+
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable (e.g., Character Name).
+[ ] 3. Define an Integer variable (e.g., Level or Age).
+[ ] 4. Define a Float variable (e.g., Speed or Power).
+[ ] 5. Define a Boolean variable (e.g., is_alive).
+[ ] 6. Print all variables to the screen using F-Strings.
+[ ] 7. Upload to GitHub and add instructor as a contributor.
+-----------------------------------------------------------------------
+"""
+# Header
+print("Custom Hero")
+print("\tBy: Niko Nardulli")
+
+# Variables
+
+character_name = "Kratos"  # String
+level = 30               # Integer
+Strength = 55.4               # Float
+is_alive = True              # Boolean
+
+# Output
+print("--- Character ---")
+print(f"Name:  {character_name}")
+print(f"Level: {level}")
+print(f"Strength:  {Strength}")
+print(f"Alive?: {is_alive}")
