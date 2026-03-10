3eaf64523d683a5a61ad3cc51e0f00db90e20d8c
NikoNard <nikonardulli@gmail.com>
2026-03-10 11:49:21 -0500

    hw

---
+++ Python/data_pass.py
@@ -0,0 +1,16 @@
+"""
+demo
+"""
+
+def greeting(name):
+    print(f"Welcome to intro programming, {name}")
+
+    #return status
+
+def main():
+    identification = input("What is your name:   ")
+    greeting(identification)
+    identification = "Quinn"
+    print(name)#would not work, out of scope
+
+main()
---
+++ Python/functions.py
@@ -0,0 +1,44 @@
+"""
+demo
+"""
+def options():
+    #give options to order, print labels, see history
+
+def menu():
+    #present menu options
+    print("menu")
+
+def order_system():
+    print("Order system")
+    #take orders from customer
+    #read in past orders and display
+    #call save function
+    menu()
+    name = input("Please enter your name:  ")
+    #read file to look for last order
+    read_orders()
+
+    save_orders()
+
+def read_orders():
+    # reads stored orders
+
+def save_orders():
+    #write to file
+    print("Save Orders")
+
+def print_labels():
+    #print on avery 2 by 3 inch labels
+    print("print labels")
+
+def calculate_cost():
+    #calculate cost (read in order)
+    print("calculate cost")
+
+def main():
+    options()
+    order_system()
+    calculate_cost()
+    print_labels()
+
+main()
