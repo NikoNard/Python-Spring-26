bd215a6bf0ffd6ced205a6d502d96a5fa5eb3fa5
NikoNard <nikonardulli@gmail.com>
2026-04-07 13:59:34 -0500

    hw

---
+++ Python/menu_reader.py
@@ -0,0 +1,54 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT 12A: THE CONFIGURABLE MENU & AUDITOR
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. PHASE 1: External menu_config.txt file created in workspace.
+[ ] 3. Program reads and parses the .txt file into a Dictionary.
+[ ] 4. PHASE 2: break the dictionary into individual variables.
+[ ] 6. Print each category and its details
+[ ] 7. try/except used to prevent crashes on FileNotFoundError.
+-----------------------------------------------------------------------
+"""
+
+def read_menu():
+    menus = {}
+
+    try:
+        with open("menu_config.txt", "r") as file:
+            for line in file:
+                parts_of_line = line.strip().split(';')
+                category = parts_of_line[0].strip()
+                detail = parts_of_line[1].strip()
+                menus[category] = detail
+
+        return menus
+    except Exception as e:
+        print(e)
+
+
+def split_into_variables(menu_items):
+
+    sizes = menu_items.get("SIZES")
+    bread = menu_items.get("BREAD")
+    protein = menu_items.get("PROTEIN")
+    cheese = menu_items.get("CHEESE")
+
+    return sizes, bread, protein, cheese
+
+def print_menu(sizes, bread, protein, cheese):
+    print("\n MENU ")
+    print(f"Sizes: {sizes}")
+    print(f"Bread: {bread}")
+    print(f"Protein: {protein}")
+    print(f"Cheese: {cheese}")
+
+
+def main():
+    menu_items = read_menu()
+    sizes, bread, protein, cheese = split_into_variables(menu_items)
+
+    print_menu(sizes, bread, protein, cheese)
+
+
+main()
